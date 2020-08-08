ca27ee1211f5d22c0dd0b346fe1f61a59b183b76
Maiya-cyber <62048625+Maiya-cyber@users.noreply.github.com>
2020-08-08 10:48:29 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const section = document.getElementById('main');
+section.style.backgroundColor=' blue';
+section.style.padding=' 0';
+section.style.margin=' 0';
+section.style.display = 'flex';
+
+
+const modal = document.querySelector(".modal");
+const openBtn = document.getElementById("btn-open");
+const closeButton = document.querySelector(".close-button");
+
+
+openBtn.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
